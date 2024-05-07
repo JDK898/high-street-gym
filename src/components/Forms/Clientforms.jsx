490029dc3fce9d4543e4034988d9a7bfbcf6aa53
JDK898 <james.daniel.king06@gmail.com>
2024-05-07 13:44:56 +1000

    made some improvements

--- src/components/Forms/Clientforms.jsx
+++ src/components/Forms/Clientforms.jsx
@@ -1,11 +1,9 @@
 // import { Link } from "react-router-dom"
 import "./Clientforms.css"
 
-function Details({inputtype, inputname, inputid}) {
+function Details({ inputtype, inputname, inputid }) {
     return (
-        <>
-        <input type={inputtype} name={inputname} id={inputid} />        
-        </>
+            <input type={inputtype} name={inputname} id={inputid} />
     )
 }
 
