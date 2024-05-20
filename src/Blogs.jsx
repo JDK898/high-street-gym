d0e3c1b67c15fc06734d8a79120b6bbd9905d6dc
JDK898 <james.daniel.king06@gmail.com>
2024-05-21 08:20:48 +1000

    added spacing to the blog posts

--- src/Blogs.jsx
+++ src/Blogs.jsx
@@ -1,5 +1,6 @@
 // import { Link } from "react-router-dom";
 import Header from "./Header";
+import "./Blogs.css"
 import Blogpost from "./components/tile/blogpost";
 import Filter from "./components/Filter";
 
@@ -8,7 +9,13 @@ function Blogs() {
         <>
             <Header />
             <Filter Filtertitle={"Write Blog"} filterpath={"/Blogcreate.jsx"}/>
-            <section className="flex justify-evenly mx-20 flex-wrap mt-10">
+            <section className="flex justify-evenly mx-20 flex-wrap mt-10" id="Blog">
+                <Blogpost Title="In my opinon" Author="Bobby Jone" Blogcontent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus alias quo harum sint, minus enim." />
+                <Blogpost Title="In my opinon" Author="Bobby Jone" Blogcontent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus alias quo harum sint, minus enim." />
+                <Blogpost Title="In my opinon" Author="Bobby Jone" Blogcontent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus alias quo harum sint, minus enim." />
+                <Blogpost Title="In my opinon" Author="Bobby Jone" Blogcontent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus alias quo harum sint, minus enim." />
+                <Blogpost Title="In my opinon" Author="Bobby Jone" Blogcontent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus alias quo harum sint, minus enim." />
+                <Blogpost Title="In my opinon" Author="Bobby Jone" Blogcontent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus alias quo harum sint, minus enim." />
                 <Blogpost Title="In my opinon" Author="Bobby Jone" Blogcontent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus alias quo harum sint, minus enim." />
                 <Blogpost Title="In my opinon" Author="Bobby Jone" Blogcontent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus alias quo harum sint, minus enim." />
                 <Blogpost Title="In my opinon" Author="Bobby Jone" Blogcontent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus alias quo harum sint, minus enim." />
