ce89ef58fcd10ca5a8eeddf7390d712451bf6f0d
JDK898 <james.daniel.king06@gmail.com>
2024-05-21 13:12:37 +1000

    Created the Footer element

---
+++ src/components/Footer.jsx
@@ -0,0 +1,33 @@
+import { Link } from "react-router-dom";
+import "./Footer.css"
+
+function Footer() {
+    return(
+        <footer className="flex justify-evenly">
+            <div className="border border-spacing-2 border-black border-solid flex flex-col flex-wrap items-center p-10 max-h-49">
+                <h3 className="text-lg">Group 1</h3>
+                <Link>This is a placeholder</Link>
+                <Link>This is a placeholder</Link>
+                <Link>This is a placeholder</Link>
+                <Link>This is a placeholder</Link>
+            </div>
+            <div className="border border-spacing-2 border-black border-solid flex flex-col flex-wrap items-center p-10 max-h-49">
+                <h3>Group 2</h3>
+                <Link>This is a placeholder</Link>
+                <Link>This is a placeholder</Link>
+                <Link>This is a placeholder</Link>
+                
+            </div>
+            <div className="border border-spacing-2 border-black border-solid flex flex-col flex-wrap items-center p-10 max-h-49">
+                <h3>Group 3</h3>
+                <Link>This is a placeholder</Link>
+                <Link>This is a placeholder</Link>
+                <Link>This is a placeholder</Link>
+                <Link>This is a placeholder</Link>
+                <Link>This is a placeholder</Link>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
