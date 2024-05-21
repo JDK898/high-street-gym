0ea929bee605819e42052fc8522b6c141d08bd89
JDK898 <james.daniel.king06@gmail.com>
2024-05-21 13:13:44 +1000

    Started to convert Styles to Tailwind classess

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -1,6 +1,7 @@
 import "./Homepage.css"
 import Header from "./Header"
 import Tile from "./components/tile/Tile"
+import Footer from "./components/Footer"
 
 function Homepage() {
     return (
@@ -8,12 +9,13 @@ function Homepage() {
         <>
             <Header />
             <div className="spacer"></div>
-            <div className="info">
+            <div className="info flex justify-between ml-10 mr-10 flex-wrap">
                 <Tile contentType="Ph: " content=" 70-- --- ---" type="AS" />
                 <Tile contentType="Email: " content=" high.street.gym@emali.com" type="AS" /> 
                 <Tile contentType="Loaction:" type="" content=" Lorem, ipsum." /> 
                 <Tile contentType="Trading hours:" type="" content="Lorem, ipsum dolor." /> 
             </div>
+            <Footer/>
 
         </>
     )
