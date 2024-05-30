4841bd8a87ed4174341f80dd1d5bc5c74d5d6a83
JDK898 <james.daniel.king06@gmail.com>
2024-05-30 12:56:43 +1000

    Spaced the input areas for both the log in and sign up pages evenly with the picture on the other side

--- src/Login.jsx
+++ src/Login.jsx
@@ -11,20 +11,30 @@ function Login() {
     return (
         <>
             <Header />
-            <div className="ml-6 mr-6">
-                <h1 className="text-3xl">Welcom Back</h1>
-                <img src="https://picsum.photos/400/400" alt="stock image" className="" />
-                {/* TODO: fix invisable sytling problem */}
-                <Details inputtype="text" inputname="Username" inputid="user" />
-                <Details inputtype="password" inputname="Password" inputid="PW" />
-                {/* TODO: add Login and join btn */}
-                {/* <input type="button" value="" content="Login"/> */}
-                <div className="Flex space-x-2 ">
-                    <button type="submit" className="border-2 border-solid border-black border-r-2">Login</button>
-                    <Link to={"/Signup"}>Join Now!</Link>
+            <section>
+                <div className="mx-10 mt-5 max-md:mx-5">
+                    <div className="flex flex-row justify-evenly mt-10">
+                        <div className="basis-1/3 mr-5 max-md:mr-0">
+                            <h1 className="text-3xl">Welcom Back</h1>
+                            <Details inputtype="text" inputname="Username" inputid="user" />
+                            <Details inputtype="password" inputname="Password" inputid="PW" />
+                            <div className="Flex space-x-2 ">
+                                <button type="submit" className="border-2 border-solid border-black border-r-2">Login</button>
+                                <Link to={"/Signup"}>Join Now!</Link>
+                            </div>
+                        </div>
+                        <div className="basis-1/3 max-md:hidden">
+
+                            <img src="src\assets\image-solid.svg" alt="stock image" className="max-w-sm" />
+                        </div>
+
+                    </div>
+                    {/* TODO: add Login and join btn */}
+                    {/* <input type="button" value="" content="Login"/> */}
                 </div>
-            </div>
-            <Footer/>
+            </section>
+
+            <Footer />
 
 
 
--- src/Signup.jsx
+++ src/Signup.jsx
@@ -8,20 +8,29 @@ function Signup() {
     return (
         <>
             <Header />
-            <h1 className="text-3xl">Welcom to the family</h1>
-            <img src="https://picsum.photos/400/400" alt="stock image" className="" />
-            <div>
-                <Details inputtype="text" inputname="First Name" inputid="user" />
-                <Details inputtype="text" inputname="Last Name" inputid="user" />
-                <Details inputtype="text" inputname="Username" inputid="user" />
-                <Details inputtype="tel" inputname="Phone Number" inputid="user" />
-                <Details inputtype="email" inputname="email" inputid="user" />
-                <Details inputtype="password" inputname="password" inputid="user" />
-            </div>
-            <div className="Flex space-x-2 ">
-                <button type="submit">Join</button>
-                <Link to={"/Login"}>Back</Link>
-            </div>
+            <section className="mx-10 mt-5 max-md:mx-5">
+
+                <div className="flex flex-row justify-evenly mt-10">
+                    <div className="basis-1/3 mr-5 max-md:mr-0">
+                        <h1 className="text-3xl">Welcom to the family</h1>
+                        <Details inputtype="text" inputname="First Name" inputid="user" />
+                        <Details inputtype="text" inputname="Last Name" inputid="user" />
+                        <Details inputtype="text" inputname="Username" inputid="user" />
+                        <Details inputtype="tel" inputname="Phone Number" inputid="user" />
+                        <Details inputtype="email" inputname="email" inputid="user" />
+                        <Details inputtype="password" inputname="password" inputid="user" />
+                        <div className="Flex space-x-2 ">
+                            <button type="submit">Join</button>
+                            <Link to={"/Login"}>Back</Link>
+                        </div>
+                    </div>
+                    <div className="basis-1/3 max-md:hidden">
+                        <img src="src\assets\image-solid.svg" alt="stock image" className="max-w-sm" />
+                    </div>
+                </div>
+
+
+            </section>
             <Footer />
 
         </>
