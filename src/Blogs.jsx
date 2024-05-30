d7c08d46c1358bce759ad8d301ee8bf6dd4f0612
JDK898 <james.daniel.king06@gmail.com>
2024-05-30 12:16:06 +1000

    Fixed the Blog create link

--- src/Blogs.jsx
+++ src/Blogs.jsx
@@ -9,8 +9,10 @@ function Blogs() {
     return (
         <>
             <Header />
-            <Filter Filtertitle={"Write Blog"} filterpath={"/Blogcreate.jsx"}/>
+            <Filter Filtertitle={"Write Blog"} filterpath={"/Blogcreate"}/>
             <section className="flex justify-evenly mx-20 flex-wrap mt-10" id="Blog">
+                <Blogpost Title="In my opinon" Author="Bobby Jone" Blogcontent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus alias quo harum sint, minus enim." />
+                <Blogpost Title="In my opinon" Author="Bobby Jone" Blogcontent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus alias quo harum sint, minus enim." />
                 <Blogpost Title="In my opinon" Author="Bobby Jone" Blogcontent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus alias quo harum sint, minus enim." />
                 <Blogpost Title="In my opinon" Author="Bobby Jone" Blogcontent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus alias quo harum sint, minus enim." />
                 <Blogpost Title="In my opinon" Author="Bobby Jone" Blogcontent="Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus alias quo harum sint, minus enim." />
