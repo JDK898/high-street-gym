06b9f2cada2bb4579fb876e2ddfabb0673187765
JDK898 <james.daniel.king06@gmail.com>
2024-06-04 09:52:01 +1000

    fixed another issue with the url in the header

--- src/Header.jsx
+++ src/Header.jsx
@@ -9,7 +9,7 @@ function Header() {
                 <Link to="/high-street-gym/">
                     <img src="images\image-regular.svg" alt="High street gym logo" className="bg-none border-none size-auto min-h-12 pl-1" />
                 </Link>
-                <Link to="/" className="flex self-center px-2 py-1 border-2 border-none rounded text-white bg-yellow-500 hover:bg-yellow-600">Home</Link>
+                <Link to="/high-street-gym/" className="flex self-center px-2 py-1 border-2 border-none rounded text-white bg-yellow-500 hover:bg-yellow-600">Home</Link>
                 <Link to="/Blogs" className="flex self-center px-2 py-1 border-2 border-none rounded text-white bg-yellow-500 hover:bg-yellow-600">Blog</Link>
                 <Link to="/Lessons" className="flex self-center px-2 py-1 border-2 border-none rounded text-white bg-yellow-500 hover:bg-yellow-600">Lessons</Link>
                 <Link to="/About" className="flex self-center px-2 py-1 border-2 border-none rounded text-white bg-yellow-500 hover:bg-yellow-600">About</Link>
