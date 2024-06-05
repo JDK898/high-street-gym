da67edffebbe77c2189fad3676233b8d21050968
JDK898 <james.daniel.king06@gmail.com>
2024-06-06 09:10:07 +1000

    fixed a styling confilic

--- src/components/Forms/Clientforms.jsx
+++ src/components/Forms/Clientforms.jsx
@@ -5,7 +5,7 @@ function Details({ inputtype, inputname, inputid }) {
     return (
         <>
         <label htmlFor={inputid} className="block">{inputname}</label>
-        <input type={inputtype} name={inputname} id={inputid} placeholder={inputname} className="border border-solid border-2 border-black" />
+        <input type={inputtype} name={inputname} id={inputid} placeholder={inputname} className=" border-solid border-2 border-black" />
         </>
         
     )
