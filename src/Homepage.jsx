c2cd74c596984c61420497c8f28850d7fab76eec
JDK898 <james.daniel.king06@gmail.com>
2024-06-06 09:04:19 +1000

    Made the Location tile on the home page redirect to google maps

--- src/Homepage.jsx
+++ src/Homepage.jsx
@@ -9,11 +9,11 @@ function Homepage() {
         <>
             <Header />
             <div className="spacer"></div>
-            <div className="info flex justify-between ml-10 mr-10 flex-wrap">
-                <Tile contentType="Ph: " content=" 70-- --- ---" type="AS" icon="src\assets\phone-flip-solid.svg" iconalt="call us at"/>
-                <Tile contentType="Email: " content=" high.street.gym@emali.com" type="AS" icon="src\assets\envelope-solid.svg" iconalt="Email us at" /> 
-                <Tile contentType="Loaction:" type="" content=" Lorem, ipsum." icon="src\assets\map-location-dot-solid.svg" iconalt="Were located at" /> 
-                <Tile contentType="Trading hours:" type="" content="Lorem, ipsum dolor." icon="src\assets\business-time-solid.svg" iconalt="Our trading hours are" /> 
+            <div className="flex justify-between mx-10 flex-wrap max-sm:flex-col">
+                <Tile contentType="Ph: " content="70-- --- ---" type={"AS"} url={"#"} icon="src\assets\phone-flip-solid.svg" iconalt="call us at"/>
+                <Tile contentType="Email:" content="high.street.gym@emali.com" url={"#"} type={"#"} icon="src\assets\envelope-solid.svg" iconalt="Email us at" /> 
+                <Tile contentType="Loaction:" url={"https://www.google.com/maps"} type={"#"} content="Lorem, ipsum." icon="src\assets\map-location-dot-solid.svg" iconalt="Were located at" /> 
+                <Tile contentType="Trading hours:" url={"#"} type={"#"} content="Lorem, ipsum dolor." icon="src\assets\business-time-solid.svg" iconalt="Our trading hours are" /> 
             </div>
             <Footer/>
 
--- src/components/tile/Tile.jsx
+++ src/components/tile/Tile.jsx
@@ -1,15 +1,18 @@
 import { Link } from "react-router-dom"
 import "./Tile.css"
 
-function Tile({ contentType, content, type, icon, iconalt }) {
+function Tile({ contentType, content, type, icon, iconalt, url }) {
     // TODO: Accessability information 
 
     return (
-        <section className="flex flex-nowrap border-2 border-black border-solid rounded-md">
-            <img src={icon} alt={iconalt} className="max-w-5"/>
-            <h3>{contentType}</h3>
-            <Link to={{ type }}>{content}</Link>
-        </section>
+        <Link to={url} className="max-sm:pt-5 ">
+            <section className="flex flex-wrap max-sm:flex-row max-sm:py-2 max-sm:max-w-xs justify-center border-2 border-black border-solid rounded-md">
+                <img src={icon} alt={iconalt} className="max-w-5" />
+                <h3 className="px-2">{contentType}</h3>
+                <Link to={type} >{content}</Link>
+            </section>
+        </Link>
+
     )
 }
 
